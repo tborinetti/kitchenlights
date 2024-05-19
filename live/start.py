46016f6f682fcab59c2647a6799ba48989826fb4
tborinetti <tborinetti@gmail.com>
2024-05-19 17:01:01 +1000

    Light detection

--- live/start.py
+++ live/start.py
@@ -3,27 +3,33 @@ from mjpeg_streamer import MjpegServer, Stream
 
 cap = cv2.VideoCapture(0)
 
-stream = Stream("camera", size=(640, 480), quality=10, fps=15)
+stream = Stream("camera", size=(640, 480), quality=50, fps=15)
 
 server = MjpegServer("192.168.1.194", 8080)
 server.add_stream(stream)
 server.start()
 
 def checkColours(img):
-	img.convert('RGB')
-	pixelArray = img.getpixel((300, 200))
-	pixelSum = pixelArray[0] + pixelArray[1] + pixelArray[2]
-
-	print(pixelSum)
-
+	dir(img)
+	pixelArray = img[320][240]
+	pixelSum = int(pixelArray[0]) + int(pixelArray[1]) + int(pixelArray[2])
+	pixelSum = pixelSum / 3
+#	print("sum: ", pixelSum)
+#	print(pixelArray)
+	return int(pixelSum)
+	
 while True:
 	_, frame = cap.read()
 	cv2.imshow(stream.name, frame)
 	if cv2.waitKey(1) == ord("q"):
 		break
 	stream.set_frame(frame)
-	checkColours(frame)
+	if checkColours(frame)  >= 70:
+		print("lights on")
+	else:
+		print("lights off")
 
+	
 
 
 server.stop()

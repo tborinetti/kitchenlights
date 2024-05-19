5c89d3283e2e01a69e85a60ebd8754e32ded4de9
tborinetti <56913002+tborinetti@users.noreply.github.com>
2024-05-19 16:34:24 +1000

    Check Colours test

--- live/start.py
+++ live/start.py
@@ -3,11 +3,18 @@ from mjpeg_streamer import MjpegServer, Stream
 
 cap = cv2.VideoCapture(0)
 
-stream = Stream("camera", size=(640, 480), quality=50, fps=30)
+stream = Stream("camera", size=(640, 480), quality=10, fps=15)
 
 server = MjpegServer("192.168.1.194", 8080)
 server.add_stream(stream)
 server.start()
+
+def checkColours(img):
+	img.convert('RGB')
+	pixelArray = img.getpixel((300, 200))
+	pixelSum = pixelArray[0] + pixelArray[1] + pixelArray[2]
+
+	print(pixelSum)
 
 while True:
 	_, frame = cap.read()
@@ -15,8 +22,10 @@ while True:
 	if cv2.waitKey(1) == ord("q"):
 		break
 	stream.set_frame(frame)
+	checkColours(frame)
+
+
 
 server.stop()
 cap.release()
 cv2.destroyAllWindows()
-
